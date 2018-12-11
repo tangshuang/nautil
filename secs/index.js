64678ec7db718bdddfe6c4311fd0a51d8e045519
ubuntu <ubuntu@localhost.localdomain>
2018-12-11 23:34:29 +0800

    update

--- secs/index.js
+++ secs/index.js
@@ -1,20 +1,18 @@
 import { Router, Render } from 'nautil'
 
-const route = new Router({
+const router = new Router({
   mode: 'history',
   views: [
     {
       name: 'sample',
       url: '/sample',
-      controller: import('./pages/sample/controller'),
-      model: import('./pages/sample/model'),
-      template: import('./pages/sample/view.html'),
-      preload: import('./pages/loading.html'),
-      stylesheet: import('./pages/sample/style.scss'),
-    }
+      component: import('./pages/sample'),
+    },
   ],
 })
-const SomeComponent = import('./components/box/index')
-const app = new Render('#app')
-app.route(route)
-app.component(SomeComponent)
+
+const app = new Render({
+  el: '#app',
+  router,
+})
+
