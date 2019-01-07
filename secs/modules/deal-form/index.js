395b28f81961b056bb702ba0e6d0c8b2ee7f719d
tisontang(唐霜) <tisontang@tencent.com>
2019-01-07 18:48:09 +0800

    delete no use code

--- secs/modules/deal-form/index.js
+++
@@ -1,7 +0,0 @@
-import { Component } from 'nautil'
-
-export default class DealFormModule extends Component {
-  static controller = import('./controller')
-  static model = import('./model')
-  static view = import('./view.jsx')
-}
