398fd34f535bef3543f917cd703f8fd19a9a32f4
tisontang(唐霜) <tisontang@tencent.com>
2019-03-26 17:39:42 +0800

    update

--- sec2/components/box.js
+++ sec2/components/box.js
@@ -1,4 +1,4 @@
-import { Component, render } from 'nautil'
+import { Component, mount, Model } from 'nautil'
 
 class Box extends Component {
 
@@ -9,8 +9,9 @@ class Box extends Component {
     weight: Number,
   }
 
-  // data初始值，会和props合并之后放到this.data上，注意，不能和props存在同名属性
-  static data = {
+  // 会和props合并之后放到this.data上，注意，不能和props存在同名属性
+  // 为了便于统一管理数据，不建议在组件内使用state，做到无状态
+  static state = {
     name: 'Tomy'
   }
 
@@ -49,12 +50,12 @@ class Box extends Component {
 }
 
 // 提供来一个color属性，但是，这个属性不能在组件内通过修改data发生变化带来响应式效果
-render(<Box size={100} weight={90} color="red" />, '#app')
+mount('#app', <Box size={100} weight={90} color="red" />)
 
 // 一个render函数内的props具备整体响应的能力
 class Container extends Component {
   static props = ['size']
-  static data = {
+  static state = {
     weight: 10,
   }
   render() {
@@ -65,4 +66,28 @@ class Container extends Component {
   }
 }
 // 在这个render内，内部Box引起的size变化，会带来Container和Box中size的同时变化
-render(<Container size={12} />, '#app2')
+mount('#app2', <Container size={12} />)
+
+// 外部更新数据
+const model = new Model({ // Model实例是响应式的
+  size: 100,
+  weight: 90,
+  color: 'red',
+})
+mount('#app3', Box, model) // => mount('#app3', <Box {...model} />)
+// 监控内部数据变化
+model.$watch('*', ({ newValue, oldValue }) => {
+  console.log(newValue, oldValue)
+})
+// 通过外部重新渲染界面。注意，要使用这种功能，model必须是nautil提供的Model的实例，并且，以第三个参数的形式去传入到mount中
+model.color = 'blue'
+
+// 还有一种外部更新数据的方式
+const data = {
+  size: 100,
+  weight: 90,
+  color: 'red',
+}
+const app = mount('#app4', Box, data)
+data.size = 120 // 外部的data发生变化不会影响内部的data，因为内部的data并非对外部data的直接引用，而外部的data又不是响应式的（内部的data变化会反馈到外部data上）
+app.update() // app.update方法重新去diff，然后渲染
