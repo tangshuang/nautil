9f4f44a52e30f04f8b587f7295158b2e0dfc4737
tisontang(唐霜) <tisontang@tencent.com>
2019-06-26 14:09:49 +0800

    update

--- src/core/component.js
+++ src/core/component.js
@@ -1,33 +1,45 @@
 import React from 'react'
 import { each } from './utils'
 
+const hoist = Symbol('hoist')
+
 export class Component extends React.Component {
   constructor(...args) {
     super(...args)
 
-    var props = { ...this.props }
-    const modify = (props) => {
-      each(props, (value, key) => {
-        if (key.indexOf('$') === 0) {
-          delete props[key]
-          const attr = key.substr(1)
-          this[attr] = value
-        }
-      })
-
-      this.children = props.children
-      delete props.children
-    }
-    modify(props)
-    Object.defineProperty(this, 'props', {
-      enumerable: false,
-      configurable: false,
-      get: () => props,
-      set: (v) => {
-        const p = { ...v }
-        modify(p)
-        props = p
-      },
-    })
+    this[hoist](this.props)
   }
+
+  [hoist](props) {
+    each(this, (value, key) => {
+      if (key.indexOf('$') === 0) {
+        delete this[key]
+      }
+    })
+    each(props, (value, key) => {
+      if (key.indexOf('$') === 0) {
+        this[key] = value
+      }
+    })
+    this.children = props.children
+  }
+
+  // Should not rewrite following methods
+  componentDidMount() {
+    this.onMounted()
+  }
+  componentDidUpdate() {
+    this.onUpdated()
+  }
+  componentWillReceiveProps(nextProps) {
+    this[hoist](nextProps)
+  }
+  shouldComponentUpdate(...args) {
+    return this.shouldUpdate(...args)
+  }
+
+  // Lifecircle
+  shouldUpdate() {}
+  onMounted() {}
+  onUpdated() {}
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+import React from 'react'
+
 export { Component } from './core/component.js'
 export { StyleSheet } from './core/stylesheet.js'
+export { Store } from './core/store.js'
+
+export { Observer } from './core/observer.js'
+export { Provider } from './core/provider.js'
+
+export { React }
