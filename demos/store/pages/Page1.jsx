02fb397fdc28d85980b249231ccaeedbdb546c72
tisontang(唐霜) <tisontang@tencent.com>
2019-06-26 14:45:04 +0800

    update

--- demos/store/pages/Page1.jsx
+++ demos/store/pages/Page1.jsx
@@ -1,13 +1,17 @@
 import { React, Component } from 'nautil'
 
 export class Page1 extends Component {
+  change() {
+    // change the sepcial prop directly, which will trigger rerender
+    this.$state.age = parseInt(Math.random() * 100, 10)
+  }
   render() {
     const state = this.$state
     return (
       <div>
         <p><span>name: {state.name}</span></p>
         <p><span>age: {state.age}</span></p>
-        <p><button onClick={() => { state.age = parseInt(Math.random() * 100, 10) }}>change</button></p>
+        <p><button onClick={() => this.change()}>change</button></p>
       </div>
     )
   }
