9fbe093593e1b052a524fe14c8ea3ef05f8a8571
tisontang(唐霜) <tisontang@tencent.com>
2019-07-18 19:12:44 +0800

    fix when factor is NaN

--- demo/pages/Page3.jsx
+++ demo/pages/Page3.jsx
@@ -18,8 +18,8 @@ export class Page3 extends Component {
         </Section>
         <Animation
           show={this.state.show}
-          enter="easeInQuint 500 fade:in move:right scale:1.5 rotate:60deg"
-          leave="easeOutQuint 500 fade:out move:0/-300,-100 scale:1/0.5 rotate:0deg/-20deg"
+          enter="easeInElastic 500 fade:in move:right,top scale:1.5/1"
+          leave="easeInQuad 500 fade:out move:0/-300 scale:1/1.5"
         >
           <Text>This is a demo for animation.</Text>
         </Animation>
