517c0f9439980c70e369c7dce40c40c69a556850
frustigor <frustigor@gmail.com>
2019-09-18 00:16:49 +0800

    update

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ export { Store } from './lib/core/store.js'
 export { Model, Schema } from './lib/core/model.js'
 export { Depository } from './lib/core/depository.js'
 export { Navigation } from './lib/core/navigation.js'
-
-export {
-  createClassName,
-  cloneElement,
-  createContext,
-  mapChildren,
-  filterChildren,
-} from './lib/core/utils.js'
