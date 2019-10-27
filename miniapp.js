ecfa2c87ff8bcbc840f3c7a0d88d64e9899cbd2a
frustigor <frustigor@gmail.com>
2019-10-28 00:26:38 +0800

    update: rename to wx-mp

--- miniapp.js
+++
@@ -1,7 +0,0 @@
-import './lib/miniapp/plyfills.js'
-import './lib/miniapp/storage.attach.js'
-import './lib/miniapp/navigation.attach.js'
-import './lib/miniapp/depository.attach.js'
-import './lib/dom/style.attach.js'
-
-export * from './lib/miniapp/miniapp.js'
