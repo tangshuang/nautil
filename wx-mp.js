678e9ce5c9db3985ba98dfd1775f74755b769133
tisontang <tisontang@tencent.com>
2019-10-31 19:51:49 +0800

    rename wx-mp to wechat-mp

--- wx-mp.js
+++
@@ -1,7 +0,0 @@
-import './lib/wx-mp/plyfills.js'
-import './lib/wx-mp/storage.attach.js'
-import './lib/wx-mp/navigation.attach.js'
-import './lib/wx-mp/depository.attach.js'
-import './lib/dom/style.attach.js'
-
-export * from './lib/wx-mp/wx-mp.js'
