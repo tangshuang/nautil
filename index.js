3e35dbd653d20440c8a9a17717819d1b29d9bdcc
tangshuang <Z40020596@zhangguangmindeMacBook-Air.local>
2020-02-10 08:55:48 +0800

    update

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ export { Model, Schema } from './lib/core/model.js'
 export { Depository } from './lib/core/depository.js'
 export { Navigation } from './lib/core/navigation.js'
 export { Storage } from './lib/core/storage.js'
+export {
+  createContext,
+  createElement,
+  createFactory,
+  createRef,
+  createTwoWayBinding,
+  Children,
+  isElement,
+} from './lib/utils.js'
