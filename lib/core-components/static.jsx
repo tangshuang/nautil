759a358c03ff7bbcb42872584bb28a2b9e4e3e38
tangshuang <mail@tangshuang.net>
2020-03-08 15:01:38 +0800

    [*] update Static component

--- lib/core-components/static.jsx
+++ lib/core-components/static.jsx
@@ -1,11 +1,18 @@
 import Component from '../core/component.js'
+import { isFunction } from '../utils.js'
+import { Any, ifexist } from '../types.js'
 
 export class Static extends Component {
+  static props = {
+    shouldUpdate: Any,
+    render: ifexist(Function),
+  }
   shouldUpdate(nextProps) {
     return nextProps.shouldUpdate
   }
   render() {
-    return this.children()
+    const { render } = this.attrs
+    return isFunction(this.children) ? this.children() : isFunction(render) ? render() : this.children
   }
 }
 export default Static
