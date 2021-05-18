d69042f61015888a86736d8a83156b55af7d30b4
tangshuang <mail@tangshuang.net>
2021-05-18 10:27:14 +0800

    fix: fix createAsyncComponent with model import

--- src/lib/create-app.js
+++ src/lib/create-app.js
@@ -5,6 +5,8 @@ import { nest } from './operators/operators.js'
 import { Ty } from 'tyshemo'
 import Navigation from './navi/navigation.js'
 import { Component } from './component.js'
+import { isFunction, isInstanceOf } from 'ts-fns'
+import { Component as ReactComponent } from 'react'
 
 export function createApp(options = {}, fn) {
   const { navigation, store, i18n } = options
@@ -37,7 +39,19 @@ export function createAsyncComponent(fn) {
   return class extends Component {
     component = null
     onMounted() {
-      fn().then(component => {
+      fn().then((res) => {
+        let component = null
+        if (res && res[Symbol.toStringTag] === 'Module') {
+          component = res.default
+        }
+        else {
+          component = res
+        }
+
+        if (!isFunction(res) && !isInstanceOf(res, ReactComponent)) {
+          throw new Error('createAsyncComponent did not receive a valid component')
+        }
+
         this.component = component
         this.update()
       })
