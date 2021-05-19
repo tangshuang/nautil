6d2bb33b4353dcf8c0110379426bfe2d312c3df9
tangshuang <mail@tangshuang.net>
2021-05-19 16:54:40 +0800

    fix: useShallowEqual dont check at the first time

--- src/lib/hooks/shallow-latest.js
+++ src/lib/hooks/shallow-latest.js
@@ -6,9 +6,16 @@ import { isShallowEqual } from '../utils.js'
  * @returns the latest shallow equal object
  */
 export function useShallowLatest(obj) {
+  const used = useRef(false)
   const latest = useRef(obj)
-  if (!isShallowEqual(latest.current, obj)) {
+
+  if (used.current && !isShallowEqual(latest.current, obj)) {
     latest.current = obj
   }
+
+  if (!used.current) {
+    used.current = true
+  }
+
   return latest.current
 }
